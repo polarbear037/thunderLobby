ecde040fd6a59d53118a09ac85aaed65d9310742
polarbear037 <140953680+polarbear037@users.noreply.github.com>
2026-05-25 00:13:14 +0800

    Add files via upload

---
+++ 1.0.2/update.js
@@ -0,0 +1,8 @@
+const UPDATE = {
+  version: "1.0.2",
+  files: [
+    "extension.js",
+    "src/main.js",
+  ],
+  intro: "增加了创建盒子观察者以及懒加载功能"
+}
---
+++ 1.0.2/update_files/src/precontent.js
@@ -0,0 +1,30 @@
+import { lib, game, ui, get, _status } from "noname";
+async function creatTL() {
+    if (lib.config.show_splash !== "off") {
+        const cfm = confirm("【雷霆】请注意：运行本扩展需关闭系统默认的启动页，点击确定为您关闭并重启，或点击取消关闭本扩展")
+        if (cfm) {
+            game.saveConfig("show_splash", "off");
+        } else {
+            game.saveConfig("extension_雷霆_enable", false);
+        }
+        game.reload();
+    }
+    if (localStorage.getItem(lib.configprefix + "directstart")) return;
+    const iframe = document.createElement('iframe');
+    iframe.id = "xl_thunderLobby";
+    iframe.src = `${lib.assetURL}extension/雷霆/app.html`;
+    iframe.style.cssText = "width:100%;height:100%;border:none;position:absolute;z-index:99";
+    document.body.appendChild(iframe);
+    const modules = { lib, game, ui, get };
+    for (const key in modules) {
+        iframe.contentWindow[key] = modules[key];
+    }
+    window.addEventListener('message', (e) => {
+        if (e.data.type === 'CLOSE_THUNDERLOBBY') {
+            const iframe = document.getElementById('xl_thunderLobby');
+            if (iframe) { iframe.remove(); }
+            if (ui.backgroundMusic) game.playBackgroundMusic();
+        }
+    });
+}
+export { creatTL as precontent }
